a86a78e1dd67e1ec5ad3c7817b317614c4da7300
navh <navarre.hebb@gmail.com>
2020-11-20 22:15:38 -0500

    add init buffer code

--- ryley.py
+++ ryley.py
@@ -2,13 +2,13 @@ import socket
 import time
 import csv
 from telnetlib import Telnet
-#import crc16
+import crc16
 
 SENSOR_IP_ADDRESS = '192.168.1.101'
 SENSOR_TELNET_PORT = 23
 SENSOR_UDPout_PORT = 49152 #magic number #Use 'IP' command over telnet to figure out where this should be going
 
-LOCAL_IP_ADDRESS = 'localhost'
+LOCAL_IP_ADDRESS = ''
 LOCAL_UDP_PORT = 5678
 
 
@@ -32,9 +32,42 @@ class udp_RecvFrame_Send_UDP_Packetizer:
     def __init__(self):
         self.sequence = 0
 
-    def bytes_for(self, command, parameters):
-        buff = bytes(bytearray.fromhex('0000044084'))
-        #crc16.crc16xmodem
+    def bytes_for(self, command, parameters = 0):
+        #commands
+        # 1 - Start Streaming, Parameter is number of samples to send, 0 = unlimited
+        # 2 - Stop Streaming, No Params
+        # 3 - Set Packet Transmission Rate, Parameter is transmission rate in µS
+        # 4 - Ping, No Params
+        # 5 - Reset Telnet Socket (useful to issue before connecting in case another session is still open)
+
+        command_bytes = int(command).to_bytes(1, byteorder='big')
+
+        sequence_bytes = int(self.sequence).to_bytes(1, byteorder='big')
+        self.sequence += 1
+        self.sequence %= 255 #Prevent overflow on the 1 byte tobytes
+
+        if (command == 1) or (command == 3):
+            parameters_bytes = int(parameters).to_bytes(4, byteorder='big')
+        else:
+            parameters_bytes = b''
+        
+        buff = command_bytes + sequence_bytes + parameters_bytes
+
+        length_bytes = int(len(buff)).to_bytes(2, byteorder='big')
+
+        length_bytes += 2 # To account for crc code added to the end
+
+        buff = length_bytes + buff
+
+        crc_code = crc16.crc16xmodem(buff)
+
+        crc_bytes = int(crc_code).to_bytes(2, byteorder='big')
+
+        buff = buff + crc_bytes
+
+        print(buff)
+
+        #buff = bytes(bytearray.fromhex('000a 00 01 00000000 f224'))
         return buff
 
 
@@ -51,7 +84,7 @@ class Communicator:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.bind((LOCAL_IP_ADDRESS,LOCAL_UDP_PORT))
 
-        init_message = self.udp_packetizer.bytes_for('ping','')
+        init_message = self.udp_packetizer.bytes_for(4)
         print(init_message)
 
         sock.sendto(init_message,(SENSOR_IP_ADDRESS,SENSOR_UDPout_PORT))
